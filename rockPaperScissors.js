5b72202937186eaf09e28dd79ac2300951696981
djiwo <djiwo.weenas@gmail.com>
2020-01-11 18:55:59 +0100

    changes to rock paper scissors.js, now works for first time

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -16,3 +16,73 @@ function computerPlay(){
     }
 }
 
+// Player selection
+function playerPlay(){
+    let playerInput = prompt('enter \'rock\', \'paper\' or \'scissors\'');
+    let playerSelectionLowerCase = playerInput.toLowerCase();
+    return playerSelectionLowerCase;
+}
+
+// Play one round of RPS
+function playRound(player, computer) {
+    if (player=='rock'){
+        if (computer=='rock'){
+            return 'It\'s a tie. Try again!1';
+        } else if (computer=='paper'){
+            computerScore++;
+            return 'You lose! Paper beats Rock';
+        } else if (computer=='scissors'){
+            playerScore++;
+            return 'You win! Rock beats scissors';
+        }
+    } else if(player=='paper'){
+        if (computer=='rock'){
+            playerScore++;
+            return 'You win! Paper beats Rock';
+        } else if (computer=='paper'){
+            return 'It\'s a tie. Try again!2';
+        } else if (computer=='scissors'){
+            computerScore++;
+            return 'You lose! Scissors beats Paper';
+        }
+    } else if(player=='scissors'){
+        if (computer=='rock'){
+            computerScore++;
+            return 'You lose! Rock beats scissors';
+        } else if (computer=='paper'){
+            playerScore++;
+            return 'You win! Scissors beats Paper';
+        } else if (computer=='scissors'){
+            return 'It\'s a tie. Try again!3';
+        }
+    } else {
+        return 'wrong playerSelection'
+    }
+}
+
+// keep on playing the game until one player has reached 3 points.
+function playGame() {
+    let x = 0;
+    while (x <5){
+        playRound(playerPlay(),computerPlay());
+        // This doesn't work with let x = computerScore+playerScore;
+        x = computerScore+playerScore;
+    }
+}
+
+
+
+// Play the actual game: 5 rounds
+playGame();
+
+// Output the results
+function results (){
+    if (playerScore > computerScore){
+        return 'You beat the computer! With '+playerScore+' rounds to '+(computerScore)+'!';
+    } else if (computerScore > playerScore){
+     return 'You lost! With '+playerScore+' rounds to '+(computerScore)+'!';
+    }
+}
+
+// alert(results());
+
